f8eae48ef53b3f6415a6c3f87ccaa470813c26c4
Mariiii <104873264+Marih935@users.noreply.github.com>
2023-02-13 20:29:28 -0300

    Add files via upload

---
+++ HTML-CSS-JS/DesafioJS/DesafioJS-03/Q04-DesafioJS.js
@@ -0,0 +1,205 @@
+const Alunos = [
+    {
+      "email": "ajgv@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0001",
+      "nome": "Alice Jéssica Giovanna Vieira",
+      "nascimento": "04/02/2000",
+      "sexo": "Feminino",
+      "naturalidade": "Petrolina - PE",
+      "turma": 0
+    },
+    {
+      "email": "amar@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0002",
+      "nome": "Alícia Mariana Ayla da Rocha",
+      "nascimento": "12/01/1998",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "cecd@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0003",
+      "nome": "Carlos Eduardo Caleb Dias",
+      "nascimento": "20/01/2003",
+      "sexo": "Masculino",
+      "naturalidade": "Olinda - PE",
+      "turma": 0
+    },
+    {
+      "email": "dmaj@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0004",
+      "nome": "Débora Marcela Andrea Jesus",
+      "nascimento": "14/01/2002",
+      "sexo": "Feminino",
+      "naturalidade": "Paulista - PE",
+      "turma": 0
+    },
+    {
+      "email": "dnn2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0005",
+      "nome": "Diego Noah Nascimento",
+      "nascimento": "27/01/2001",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "erm3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0006",
+      "nome": "Eliane Rosa Monteiro",
+      "nascimento": "15/01/2000",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "ehn1@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0007",
+      "nome": "Ester Heloisa das Neves",
+      "nascimento": "18/01/2003",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "gdv2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0008",
+      "nome": "Gabrielly Flávia dos Santos",
+      "nascimento": "09/02/2003",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "hed1@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0009",
+      "nome": "Hugo Erick Drumond",
+      "nascimento": "01/02/1999",
+      "sexo": "Masculino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    },
+    {
+      "email": "iar3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0010",
+      "nome": "Ian Augusto Rezende",
+      "nascimento": "07/02/2003",
+      "sexo": "Masculino",
+      "naturalidade": "Olinda - PE",
+      "turma": 0
+    },
+    {
+      "email": "jes3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0011",
+      "nome": "Juan Emanuel Santos",
+      "nascimento": "22/01/2002",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "jmnc@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0012",
+      "nome": "Juan Miguel Noah da Costa",
+      "nascimento": "15/01/2002",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "maln@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0013",
+      "nome": "Marlene Antônia Lavínia Nunes",
+      "nascimento": "09/01/1996",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "mcc5@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0014",
+      "nome": "Melissa Cristiane Castro",
+      "nascimento": "09/02/2000",
+      "sexo": "Feminino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    },
+    {
+      "email": "nhm2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0015",
+      "nome": "Nelson Henry Mendes",
+      "nascimento": "06/02/2001",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "rtyl@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0016",
+      "nome": "Ricardo Theo Yuri Lopes",
+      "nascimento": "01/02/2001",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "sdc3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0017",
+      "nome": "Samuel Danilo Costa",
+      "nascimento": "01/02/1995",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "scb2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0018",
+      "nome": "Sandra Carolina Barbosa",
+      "nascimento": "03/02/2003",
+      "sexo": "Feminino",
+      "naturalidade": "Paulista - PE",
+      "turma": 0
+    },
+    {
+      "email": "tchm@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0019",
+      "nome": "Thiago Caleb Heitor Moreira",
+      "nascimento": "21/01/2002",
+      "sexo": "Masculino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    },
+    {
+      "email": "tcer@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0020",
+      "nome": "Tiago Cauã Elias Rocha",
+      "nascimento": "08/02/2000",
+      "sexo": "Masculino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    }
+]
+
+function aniversariantes(mes, Alunos) {
+    let saida = []
+    for (let i = 0; i < Alunos.length; i++) {
+        let data = Alunos[i].nascimento.split('/') //separa a data em dia e mes saida: [dia, mes, ano]
+        if (Number(data[1]) === mes) {//se o mes for igual ao mes passado como parametro
+            saida.push({data: `${data[0]}/${data[1]}`, nome: Alunos[i].nome})
+        }
+    }
+    //ordenar o array
+    saida.sort((a, b) => {
+        if (a.data > b.data) {
+            return 1
+        } else if (a.data < b.data) {
+            return -1
+        } else {
+            return 0
+        }
+    })
+    return saida
+}
+
+console.log(aniversariantes(2, Alunos))
---
+++ HTML-CSS-JS/DesafioJS/DesafioJS-03/Q09-DesafioJS.js
@@ -0,0 +1,249 @@
+const Alunos = [
+    {
+      "email": "ajgv@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0001",
+      "nome": "Alice Jéssica Giovanna Vieira",
+      "nascimento": "04/02/2000",
+      "sexo": "Feminino",
+      "naturalidade": "Petrolina - PE",
+      "turma": 0
+    },
+    {
+      "email": "amar@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0002",
+      "nome": "Alícia Mariana Ayla da Rocha",
+      "nascimento": "12/01/1998",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "cecd@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0003",
+      "nome": "Carlos Eduardo Caleb Dias",
+      "nascimento": "20/01/2003",
+      "sexo": "Masculino",
+      "naturalidade": "Olinda - PE",
+      "turma": 0
+    },
+    {
+      "email": "dmaj@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0004",
+      "nome": "Débora Marcela Andrea Jesus",
+      "nascimento": "14/01/2002",
+      "sexo": "Feminino",
+      "naturalidade": "Paulista - PE",
+      "turma": 0
+    },
+    {
+      "email": "dnn2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0005",
+      "nome": "Diego Noah Nascimento",
+      "nascimento": "27/01/2001",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "erm3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0006",
+      "nome": "Eliane Rosa Monteiro",
+      "nascimento": "15/01/2000",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "ehn1@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0007",
+      "nome": "Ester Heloisa das Neves",
+      "nascimento": "18/01/2003",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "gdv2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0008",
+      "nome": "Gabrielly Flávia dos Santos",
+      "nascimento": "09/02/2003",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "hed1@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0009",
+      "nome": "Hugo Erick Drumond",
+      "nascimento": "01/02/1999",
+      "sexo": "Masculino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    },
+    {
+      "email": "iar3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0010",
+      "nome": "Ian Augusto Rezende",
+      "nascimento": "07/02/2003",
+      "sexo": "Masculino",
+      "naturalidade": "Olinda - PE",
+      "turma": 0
+    },
+    {
+      "email": "jes3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0011",
+      "nome": "Juan Emanuel Santos",
+      "nascimento": "22/01/2002",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "jmnc@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0012",
+      "nome": "Juan Miguel Noah da Costa",
+      "nascimento": "15/01/2002",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "maln@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0013",
+      "nome": "Marlene Antônia Lavínia Nunes",
+      "nascimento": "09/01/1996",
+      "sexo": "Feminino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "mcc5@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0014",
+      "nome": "Melissa Cristiane Castro",
+      "nascimento": "09/02/2000",
+      "sexo": "Feminino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    },
+    {
+      "email": "nhm2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0015",
+      "nome": "Nelson Henry Mendes",
+      "nascimento": "06/02/2001",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "rtyl@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0016",
+      "nome": "Ricardo Theo Yuri Lopes",
+      "nascimento": "01/02/2001",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "sdc3@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0017",
+      "nome": "Samuel Danilo Costa",
+      "nascimento": "01/02/1995",
+      "sexo": "Masculino",
+      "naturalidade": "Belo Jardim - PE",
+      "turma": 0
+    },
+    {
+      "email": "scb2@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0018",
+      "nome": "Sandra Carolina Barbosa",
+      "nascimento": "03/02/2003",
+      "sexo": "Feminino",
+      "naturalidade": "Paulista - PE",
+      "turma": 0
+    },
+    {
+      "email": "tchm@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0019",
+      "nome": "Thiago Caleb Heitor Moreira",
+      "nascimento": "21/01/2002",
+      "sexo": "Masculino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    },
+    {
+      "email": "tcer@discente.ifpe.edu.br",
+      "matricula": "20222MTFS0020",
+      "nome": "Tiago Cauã Elias Rocha",
+      "nascimento": "08/02/2000",
+      "sexo": "Masculino",
+      "naturalidade": "Recife - PE",
+      "turma": 0
+    }
+]
+
+// Elabore uma função que recebe o array de alunos descrito acima e retorne um array de objetos contendo os semestres de entrada dos alunos, a quantidade e o percenctual de alunos deste semestre ( conforme o exemplo).
+
+// Nome da função: semestres_entrada
+
+// Paramêtros: array [{}, {}, ...]
+
+// Retorno: {}
+
+// exemplo 1:
+
+// Entrada: Alunos da turma 02 do Mouratech
+
+// Saída:
+
+//     [
+//         { semestre: '2019.2', quantidade: 4, percentual: '20.00 %' },
+//         { semestre: '2021.1', quantidade: 5, percentual: '25.00 %' },
+//         { semestre: '2022.1', quantidade: 9, percentual: '45.00 %' },
+//         { semestre: '2022.2', quantidade: 1, percentual: '5.00 %' },
+//         { semestre: '2023.1', quantidade: 1, percentual: '5.00 %' }
+//     ]
+
+function semestres_entrada(Alunos) {
+  let semestres = [];
+  for (let i = 0; i < Alunos.length; i++) {
+    let ano = Alunos[i].matricula.substring(0, 4);
+    let periodo = Alunos[i].matricula.substring(4, 5);
+    let semestre = ano + "." + periodo;
+    semestres.push(semestre);
+  }
+
+  let semestres_unicos = semestres.filter((item, index) => {//retorna um array sem elementos repetidos
+    return semestres.indexOf(item) === index;
+  });
+
+  let semestres_entrada = [];
+  for (let i = 0; i < semestres_unicos.length; i++) {
+
+    let quantidade = 0;
+    for (let j = 0; j < semestres.length; j++) {
+      if (semestres_unicos[i] === semestres[j]) {
+        quantidade++;
+      }
+    }
+
+    let percentual = ((quantidade / semestres.length) * 100).toFixed(2) + " %";
+    semestres_entrada.push({
+      semestre: semestres_unicos[i],
+      quantidade: quantidade,
+      percentual: percentual,
+    });
+  }
+  //ordenar o array de objetos por semestre
+  semestres_entrada.sort(function (a, b) {
+    if (a.semestre > b.semestre) {
+      return 1;
+    }
+    if (a.semestre < b.semestre) {
+      return -1;
+    }
+    return 0;
+  });
+  return semestres_entrada;
+}
+
+console.log(semestres_entrada(Alunos));
